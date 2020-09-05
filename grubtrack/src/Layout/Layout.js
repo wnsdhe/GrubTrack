815fe0d3674fabab7150bdda7dad347253208e53
Joonoh Chun <wnsdhe@gmail.com>
2020-09-05 01:00:53 -0400

    Routed dashboard to layout

--- grubtrack/src/Layout/Layout.js
+++ grubtrack/src/Layout/Layout.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './Layout.css';
+import Switch from 'react-bootstrap/esm/Switch';
+import Dashboard from '../Dashboard/Dashboard';
+import { Route } from 'react-router-dom';
 
 class Layout extends Component {
   // constructor(props){
@@ -18,7 +21,15 @@ class Layout extends Component {
 
   render() {
     return (
-      <div>This is s layoutnpm</div>
+      <div>
+        <div>
+          this is a layout
+        </div>
+        <Switch>
+          <Route path="/" exact component={Dashboard}/>
+          {/* <Route path="/login"/> */}
+        </Switch>
+      </div>
     );
   }
 }
