66ef27487a09e9753f38dd04853b529b43e229c5
Joonoh Chun <wnsdhe@gmail.com>
2020-09-22 01:37:38 -0400

    Added temporary UserModel

---
+++ Server/Server/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Server.Models
+{
+    public class UserModel : IdentityUser<int>
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
