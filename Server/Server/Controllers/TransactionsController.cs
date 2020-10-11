fd0923e899bffff5d9cd48cb904486d6a7e56088
hatsunemorioka <h.morioka0102@gmail.com>
2020-10-11 01:03:55 -0400

    Update TransactionsController.cs

--- Server/Server/Controllers/TransactionsController.cs
+++ Server/Server/Controllers/TransactionsController.cs
@@ -26,67 +26,82 @@ namespace Server.Controllers
         // GET: api/transactions
         [HttpGet]
         [Authorize]
-        public async Task<ActionResult<IEnumerable<Transactions>>> Get()
+        public async Task<ActionResult<IEnumerable<Transactions>>> Get(Guid user_id)
         {
             var values = await _context.Transactions.ToListAsync();
-            return Ok(values);
+            List<Transactions> filteredValues = new List<Transactions>();
+            foreach (var val in values)
+            {
+                if (val.userID == user_id)
+                {
+                    filteredValues.Add(val);
+                }
+            }
+            return Ok(filteredValues);
         }
 
         // GET api/transactions/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Transactions>> Get(int id)
+        public async Task<ActionResult<Transactions>> Get(int id, Guid user_id)
         {
             var value = await _context.Transactions.FindAsync(id);
-            return Ok(value);
+            if (value.userID == user_id)
+            {
+                return Ok(value);
+            }
+            else
+            {
+                return Ok();
+            }
         }
 
         // POST api/transactions
         [HttpPost]
         // public async void Post([FromBody] Transactions transaction)
-        public async Task<ActionResult<Transactions>> Post([FromBody] Transactions transaction)
+        public async Task<ActionResult<Transactions>> Post(Guid user_id, [FromBody] Transactions transaction)
         {
-            transaction.Date = System.DateTime.Now;
-            _context.Transactions.Add(transaction);
-            await _context.SaveChangesAsync();
-            return Ok(transaction);
+            if (transaction.userID == user_id)
+            {
+                transaction.Date = System.DateTime.Now;
+                _context.Transactions.Add(transaction);
+                await _context.SaveChangesAsync();
+                return Ok(transaction);
+            }
+            return Ok();
         }
 
         // PUT api/transactions/5
         [HttpPut("{id}")]
-        public async Task<ActionResult<Transactions>> Put(int id, [FromBody] Transactions transaction)
+        public async Task<ActionResult<Transactions>> Put(int id, Guid user_id, [FromBody] Transactions transaction)
         //public async void Put(DateTime dateTime, Boolean foodWaste, Boolean pickUp, int amountlbs, string status, Boolean flag)
         {
-            /* var transaction = new Transactions
-            {
-                Date = System.DateTime.Now,
-                FoodWaste = foodWaste,
-                Pickup = pickUp,
-                Amountlbs = amountlbs,
-                Status = status,
-                Flag = flag
-            }; */
-            var tr = await _context.Transactions.FindAsync(id);
+            var value = await _context.Transactions.FindAsync(id);
             //tr.Date = System.DateTime.Now;
-            tr.FoodWaste = transaction.FoodWaste;
-            tr.Pickup = transaction.Pickup;
-            tr.Amountlbs = transaction.Amountlbs;
-            tr.Status = transaction.Status;
-            tr.Flag = transaction.Flag;
-            tr.userID = transaction.userID;
-
-            //_context.Add(transaction);
-            await _context.SaveChangesAsync();
-            return Ok(tr);
+            if (value.userID == user_id)
+            {
+                value.FoodWaste = transaction.FoodWaste;
+                value.Pickup = transaction.Pickup;
+                value.Amountlbs = transaction.Amountlbs;
+                value.Status = transaction.Status;
+                value.Flag = transaction.Flag;
+                value.userID = transaction.userID;
+                await _context.SaveChangesAsync();
+                return Ok(value);
+            }
+            return Ok();
         }
 
         // DELETE api/transactions/5
         [HttpDelete("{id}")]
         //public async void Delete(int id)
-        public async Task<ActionResult<Transactions>> Delete(int id)
+        public async Task<ActionResult<Transactions>> Delete(int id, Guid user_id)
         {
-            var transaction = await _context.Transactions.FindAsync(id);
-            _context.Transactions.Remove(transaction);
-            await _context.SaveChangesAsync();
+            var value = await _context.Transactions.FindAsync(id);
+            if (value.userID == user_id)
+            {
+                _context.Transactions.Remove(value);
+                await _context.SaveChangesAsync();
+            }
             return Ok();
         }
     }
